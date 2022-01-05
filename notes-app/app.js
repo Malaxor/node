ff09e8df46f2d47ab06b2368d5c99a10566526f4
Philip Costache <nogobaduk@gmail.com>
2022-01-05 12:41:50 -0500

    note-app: installed yargs and configured a commands object with 4 commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 const { log } = console;
 
-const command = process.argv[2];
+// customize yargs version
+yargs.version('1.1.0');
 
-if (command === 'add') {
-  log('Adding note!');
-} else if (command === 'remove') {
-  log('Removing note!');
-}
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a note',
+  handler() {
+    log('Adding a note.')
+  }
+});
+
+// create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'remove a note',
+  handler() {
+    log('Removing the note.')
+  }
+});
+
+// create read command
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler() {
+    log('reading the note.')
+  }
+});
+
+// create list command
+yargs.command({
+  command: 'list',
+  describe: 'list all the notes',
+  handler() {
+    log('listing all the notes.')
+  }
+});
+
+// add, remove, read, list
+log(yargs.argv)
