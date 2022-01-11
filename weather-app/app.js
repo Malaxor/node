6b6bb0cc6186ef3e06b0fb8605e758f17d52eec8
Philip Costache <nogobaduk@gmail.com>
2022-01-11 14:38:21 -0500

    weather-app: customizing http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require('request');
 const { log } = console;
 
-const url = 'http://api.weatherstack.com/current?access_key=4470fc804c8962986009bddb66213765&query=37.8267,-122.4233';
-request({ url }, (err, res) => {
-  const data = JSON.parse(res.body);
-  log(data.current)
+const accessKey = '4470fc804c8962986009bddb66213765';
+let units = 'f';
+const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=37.8267,-122.4233&units=${units}`;
+
+request({ url, json: true }, (err, res) => {
+  const { temperature, feelslike, weather_descriptions } = res.body.current;
+  log(`${weather_descriptions[0]}. The temperature is ${temperature} degrees. It feels like ${feelslike} degrees.`);
 });
