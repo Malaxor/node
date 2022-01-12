51334938c61e7e0c226892800316dcb4f6764c58
Philip Costache <nogobaduk@gmail.com>
2022-01-12 16:17:02 -0500

    weather-app: serving HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello express!');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('help page');
+  res.send([{ name: 'Philip'}, { name: 'Christine' }]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('about page');
+  res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('weather page');
+  res.send({ forecast: 'cloudy', location: 'Detroit' });
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
