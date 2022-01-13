46a5f36493c3069af213fddb1dc5726da0f1af3e
Philip Costache <nogobaduk@gmail.com>
2022-01-13 14:21:36 -0500

    weather-app: installed, configured handlebars, including the three views and related routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
 const publicDirectoryPath = path.join(__dirname, '../public');
-app.use(express.static(publicDirectoryPath, { extensions: ['html']}));
+// the about and help html pages are viewed at /about and /index by adding the extensions object
+// the index html is served without needing the extensions object
+app.use(express.static(publicDirectoryPath, { extensions: ['html'] }));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Philip Costache'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Philip Costache'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'this is an example message for the help page.'
+  });
+});
+
 
 app.get('/weather', (req, res) => {
   res.send({ forecast: 'cloudy', location: 'Detroit' });
