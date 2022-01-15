2151e7d797cd4693519bf61378b9087c0558ac96
Philip Costache <nogobaduk@gmail.com>
2022-01-15 16:38:25 -0500

    weather-app: created search form; fetching data from server

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,4 +1,12 @@
-fetch('http://localhost:3000/weather?address=Detroit')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const { value: location } = search;
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+});
