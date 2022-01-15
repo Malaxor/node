c4fa6b9e41974047f2568b6568cd808a3a7e2f86
Philip Costache <nogobaduk@gmail.com>
2022-01-15 15:35:58 -0500

    weather-app: fetching data from the weather route with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,4 @@
-console.log('asdofiuasofasdf')
+fetch('http://localhost:3000/weather?address=Detroit')
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
