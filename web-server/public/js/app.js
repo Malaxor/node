87ad1595675d45aa094501df0a4cc64cd2f15f8f
Philip Costache <nogobaduk@gmail.com>
2022-01-15 17:09:42 -0500

    weather-app: application running

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,26 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   const { value: location } = search;
+  
+  messageOne.textContent = 'loading...'
+  messageTwo.textContent = '';
 
   fetch(`http://localhost:3000/weather?address=${location}`)
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    .then(({ error, location, weatherMsg }) => {
+      if (error) {
+        messageOne.textContent = error;
+        setTimeout(() =>{
+          messageOne.textContent = '';
+        }, 2500)
+      } else {
+        messageOne.textContent = `${location}.`;
+        messageTwo.textContent = weatherMsg;
+      }
+    });
 });
