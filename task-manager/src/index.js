b1dfee5c279553e4c13382916f2ce05174bf49a6
Philip Costache <nogobaduk@gmail.com>
2022-01-18 18:14:22 -0500

    task-app: created get all tasks and get task by id routes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,23 +18,40 @@ app.get('/users/:id', (req, res) => {
     if (!user) {
       return res.sendStatus(404);
     }
-    res.send(user)
+    res.send(user);
   })
   .catch(err => res.status(500).send(err));
 });
 
-app.post("/users", (req, res) => {
+app.post('/users', (req, res) => {
   const user = new User(req.body);
   user.save()
   .then(user => res.status(201).send(user))
   .catch(err => res.status(400).send(err));
 });
 
-app.post("/tasks", (req, res) => {
+app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save()
   .then(task => res.status(201).send(task))
   .catch(err => res.status(400).send(err));
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+  .then(tasks => res.send(tasks))
+  .catch(err => res.sendStatus(500));
+});
+
+app.get('/tasks/:id', (req, res) => {
+  Task.findById(req.params.id)
+  .then(task => { 
+    if (!task) {
+      return res.sendStatus(404);
+    }
+    res.send(task);
+  })
+  .catch(err => res.status(500).send(err));
 });
 
 app.listen(port, () => console.log('listening on port ' + port));
