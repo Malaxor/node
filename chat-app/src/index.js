87ca585671a04f1f5df48fca0eaa6198aa117f55
Philip Costache <nogobaduk@gmail.com>
2022-03-27 19:14:07 -0400

    chat-app initial setup

---
+++ chat-app/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, console.log(`server listening on port ${port}`));
