6d9e13b0994a27ce9f92f707387dbcd7eaa96c40
Philip Costache <nogobaduk@gmail.com>
2022-04-14 20:37:14 -0400

    chat-app: emitting events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,9 @@
-io();
+const socket = io();
+
+socket.on('countUpdated', (count) => {
+  console.log('the count is updated:', count);
+});
+
+document.getElementById('increment').addEventListener('click', function () {
+  socket.emit('increment');
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,17 +1,24 @@
 const path = require('path');
 const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
 const app = express();
-const server = http.createServer(app);
+const socketio = require('socket.io');
+const server = require('http').createServer(app);
 const io = socketio(server);
 const port = 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-  console.log('new web socket connection');
+let count = 0;
+io.on('connection', (socket) => {
+  socket.on('increment', () => {
+    count++;
+    // emit in real time only to the client that triggers the event
+    // socket.emit('countUpdated', count);
+    
+    // emit in real time to all clients
+    io.emit('countUpdated', count);
+  });
 });
 
 server.listen(port, console.log(`server listening on port ${port}`));
